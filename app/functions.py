14d60e9559500228b9dd4c1c0e09e244daf76133
Gabriel Pires de Barros <gdbarros95@gmail.com>
2024-10-10 23:20:30 +0000

    created functions.py file, created function turno_atual that returns the current shift

---
+++ app/functions.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+import pytz
+
+def turno_atual(return_int=False):
+    tz = pytz.timezone('America/Sao_Paulo')
+    hora_atual = datetime.now(tz).hour
+    if 6 <= hora_atual < 12:
+        return 'manha' if not return_int else 1
+    elif 12 <= hora_atual < 18:
+        return 'tarde' if not return_int else 2
+    elif 18 <= hora_atual < 22:
+        return 'noite' if not return_int else 3
+    else:
+        return 'madrugada' if not return_int else 0
+
+turno_atual()
