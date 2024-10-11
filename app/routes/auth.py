a9c74e03b8f68d519ec596182917a6d77952cb27
Kerubin <162385508+KerubinDev@users.noreply.github.com>
2024-10-11 00:29:14 +0000

    alterações no base.html

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash, jsonify, session
 from models import *
+from functools import wraps
 from werkzeug.security import check_password_hash, generate_password_hash
 import logging
-from urllib.parse import urlparse, urljoin
 
 auth = Blueprint('auth', __name__)
 
@@ -31,30 +31,23 @@ def admin_required(f):
     return decorated_function
 
 
-def is_safe_url(target):
-    ref_url = urlparse(request.host_url)
-    test_url = urlparse(urljoin(request.host_url, target))
-    return test_url.scheme in ('http', 'https') and ref_url.netloc == test_url.netloc
-
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        email = request.form['email']
-        senha = request.form['password']
+        email = request.form.get('email')
+        senha = request.form.get('password')
         next_url = request.form.get('next') or url_for('main.home')
         logger.debug(f"Tentativa de login para o email: {email}")
+        
         usuario = Usuario.query.filter_by(Email=email).first()
-        if usuario and usuario.Senha == senha:
+        if usuario and usuario.Senha == senha:  # Comparação direta da senha
             session['user_id'] = usuario.ID_usuario
             logger.info(f"Login bem-sucedido para o usuário: {usuario.Nome}")
-            if is_safe_url(next_url):
-                return redirect(next_url)
-            else:
-                return redirect(url_for('main.home'))
+            return jsonify({'success': True, 'redirect': next_url})
         else:
             logger.warning(f"Falha no login para o email: {email}")
-            flash('Credenciais inválidas. Por favor, tente novamente.', 'error')
-            return redirect(url_for('auth.login'))
+            return jsonify({'success': False, 'message': 'Credenciais inválidas. Por favor, tente novamente.'})
+    
     next_url = request.args.get('next', url_for('main.home'))
     return render_template('login.html', next=next_url)
 
@@ -63,26 +56,6 @@ def logout():
     session.pop('user_id', None)
     flash('Você foi desconectado.', 'info')
     return redirect(url_for('main.home'))
-
-@auth.route('/register-student', methods=['GET', 'POST'])
-def register_student():
-    if request.method == 'POST':
-        nome = request.form['nome']
-        email = request.form['email']
-        senha = request.form['senha']
-        
-        if Usuario.query.filter_by(Email=email).first():
-            flash('Email já cadastrado.', 'error')
-            return redirect(url_for('auth.register_student'))
-        
-        novo_aluno = Usuario(Nome=nome, Cargo='Aluno', Email=email, Senha=generate_password_hash(senha))
-        db.session.add(novo_aluno)
-        db.session.commit()
-        
-        flash('Cadastro realizado com sucesso!', 'success')
-        return redirect(url_for('auth.login'))
-    
-    return render_template('register_student.html')
 
 @auth.route('/schedule', methods=['GET', 'POST'])
 def schedule():
