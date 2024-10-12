40f433c7633a7ba965d759f802a693ee7819fcc4
Kerubin <162385508+KerubinDev@users.noreply.github.com>
2024-10-12 00:02:52 +0000

    Função de esconder icon de logado feito e removido o grafico da home

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,11 +2,9 @@ from .main import main
 from .auth import auth
 from .admin import admin
 from .test import test
-from .data_register import data_register
 
 def register_blueprints(app):
     app.register_blueprint(main)
     app.register_blueprint(auth)
     app.register_blueprint(admin)
     app.register_blueprint(test)
-    app.register_blueprint(data_register)
